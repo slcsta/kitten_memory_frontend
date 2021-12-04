79fefb3fea4f5901d800669685bf1a65ac4723ab
Sarah <sarahlchavez@gmail.com>
2021-12-04 11:39:09 -0800

    added reducer to store

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 // what do i need from the redux library to create my store?
 import { applyMiddleware, createStore } from "redux";
 import thunk from "redux-thunk";
+import gamesReducer from "./reducers/gamesReducer";
 // just exporting the return value here and it's just a default so don't need a const but now import in index
-export default createStore(reducer, applyMiddleware(thunk)); 
+export default createStore(gamesReducer, applyMiddleware(thunk)); 
 // after this go and define our reducer
