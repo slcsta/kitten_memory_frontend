26c151ca85d68bb026dbb638f7c322d42ddf2d51
Sarah <sarahlchavez@gmail.com>
2021-12-07 05:13:38 -0800

    adding pseudocode and updating card components

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,20 @@
-// main logic of the game
-// images for cards
-// sorting for cards at start of the game
-// all the click events for the cards
+// add images for cards from db
+// make cards within game board
+// sort function for cards at start of the game
+// all click events for cards go here
 
-// user clicks on 1st card
-// flip card
+// How Does the Game Work?
+// user clicks on 1st card - click event
+// flip card - on click, card should flip
 // leave card flipped
-// user clicks on second card
-// flip card
-// check card images - if 1st card same as 2nd card image, leave cards flipped
+// player clicks on second card - click event
+// flip card - on click, card should flip
+// check card images - when two click/flip events happen check for a match 
+// if 1st card same as 2nd card image, leave cards flipped
 // if images are different, flip both cards back over 
 // repeat until no more cards left to be flipped
+// show a congratulatory message game is done - stop timer
+// user can click button to start game over
 
 // use one card and dynamically populate our cards
 // how do i pass props into a functional component?
