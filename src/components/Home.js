04777030ec6d1226e3b21be28c937243b1d7000c
Sarah <sarahlchavez@gmail.com>
2021-12-12 13:38:04 -0800

    added home component

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+    render() {
+        return (
+            <div className= " button">
+                <h1> Let's Match Some Cute Kittens! </h1>
+            </div>
+        )
+    }
+}
+
+export default Home
