98b19f1073f3bc9699e526077da0d46a5711a4db
Sarah <sarahlchavez@gmail.com>
2021-12-12 12:15:17 -0800

    updated state in cards container component

--- src/components/card/CardsContainer.js
+++ src/components/card/CardsContainer.js
@@ -1,17 +1,18 @@
 // container that does data fetching and then renders images
-// making this image container dynamic 
-
-import { render } from '@testing-library/react';
+// making this image container dynamic
+//import { render } from '@testing-library/react';
 import React from 'react';
 import cardImages from '../../database';
-//import Card from './Card';
+import Card from './Card';
 
 class CardsContainer extends React.Component () {
     // add empty cards array for pushing cards after they are checked
     cards = [] 
-    // state ={
-    //     Turns: 0,
-    // }
+    state = {
+        cards,
+        turns: 0,
+        clicked: false
+    };
     //const cards = cardsObj.cardImages.map(card => < Card key={card.id} image={card.image} /> )
 
     // add onClick to handle function of clicking cards
