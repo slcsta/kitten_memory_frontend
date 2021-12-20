975dc1a2e5f8f7660ae4becdf69680657a42943c
Sarah <sarahlchavez@gmail.com>
2021-12-20 14:56:41 -0800

    added all links tto navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,16 @@
 // one of my five stateless components
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Navbar extends Component {
     render() {
         return (
-            <div >
-                <h1>  </h1>
-            </div>
+            <ul>
+                <li><Link to="/">Welcome</Link></li>
+                <li><Link to="/instructions">Instructions</Link></li>
+                <li><Link to="/cards">Cards</Link></li>
+                <li><Link to="/scoreboard">Score Board</Link></li>
+            </ul>
         )
     }
 }
