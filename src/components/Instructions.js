f9177c7ef0082432a1eed8b10a782f35968b36c2
Sarah <sarahlchavez@gmail.com>
2021-12-21 05:16:17 -0800

    added text to instructions component and styled with css

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -6,7 +6,23 @@ class Instructions extends Component {
     render() {
         return (
             <div>
-                <h1> This is how you play the game </h1>
+                <h2> How to Play </h2>
+                    <div className="instructions">
+                    <p>Find all pairs of matching cards in the least amount of turns 
+                    possible.</p>
+
+                    <p>Start by clicking on a card to flip it. Next, click on a second 
+                    card to find a match. Cards that match will remain flipped. Cards 
+                    that don't match will flip back down.</p>
+
+                    <p>Every attempt is counted as a turn. When all matches are found, 
+                    the game ends and the player can enter a name to record the score.</p> 
+
+                    <p>To start a new game at any time, click on the "play game" button. 
+                    Only fully completed games will have their scores saved as the number
+                    of turns taken to match all cards.
+                    </p>
+                </div>
             </div>
         )
     }
